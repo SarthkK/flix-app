bba9d6dacf5d479d191b3e4dfb2226ebdcd2aa51
Sarthk Kharwal <sarthkkharwal@gmail.com>
2024-02-07 15:43:48 +0530

    added sliding movies to front page using swiper library

--- js/script.js
+++ js/script.js
@@ -195,6 +195,47 @@ async function fetchAPIData(endpoint){
     return data;
 }
 
+// Display slider movies
+async function displaySlider(){
+    const { results } = await fetchAPIData('movie/now_playing');
+    results.forEach(result => {
+        const div = document.createElement('div');
+        div.classList.add("swiper-slide");
+        div.innerHTML = 
+        `<a href="movie-details.html?id=${result.id }">
+            <img src="https://image.tmdb.org/t/p/w500${result.poster_path}" alt="${result.title }" />
+        </a>
+        <h4 class="swiper-rating">
+            <i class="fas fa-star text-secondary"></i> ${result.vote_average.toFixed(1)} / 10
+        </h4>`;
+        document.querySelector('.swiper-wrapper').appendChild(div);
+        initSwiper();
+    })
+}
+function initSwiper(){
+    const swiper = new Swiper('.swiper', {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        freeMode: true,
+        loop: true,
+        autoplay: {
+            delay: 4000,
+            displayOnInteraction: false
+        },
+        breakpoints: {
+            500: {
+               slidesPerView: 2
+            },
+            700: {
+               slidesPerView: 3
+            },
+            1200: {
+               slidesPerView: 4
+            }
+        }
+    });
+}
+
 function showSpinner(){
     document.querySelector('.spinner').classList.add('show');
 }
@@ -245,6 +286,7 @@ function init(){
     switch (global.currentPath) {
         case '/' :
         case '/index.html':
+            displaySlider();
             displayPopularMovies();
             break;
         case '/shows.html' :
